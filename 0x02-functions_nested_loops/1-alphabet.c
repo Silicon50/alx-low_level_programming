1a9a484c5dff641e9cabe4373684cbe67bc36815
Silicon50 <ndukwechukwuaja@gmail.com>
2022-09-14 01:06:29 +0100

    print abc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print function abc
+ *
+ * Description: print alphabets in lower case
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+}
