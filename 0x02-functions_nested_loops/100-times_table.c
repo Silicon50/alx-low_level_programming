02bb1d12a2e321d1259b489b948e171197d1eaeb
Silicon50 <ndukwechukwuaja@gmail.com>
2022-09-15 00:40:35 +0100

    just tring

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,11 +10,42 @@
 
 void print_times_table(int n)
 {
-	for (n = 1; n >= 0; n++)
+	int x = 0, y, z;
+
+	if (n > 15 || n < 0)
+	return;
+	while (x <= n)
 	{
-		if (n > 15 || n < 0)
-			break;
-		_putchar(n);
-		_putchar('\n');
-	}
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+																		}
+				else if (z > 9)
+				{
+					_putchar(' ');
+					_putchar(z / 10 + '0');
+					_putchar(z % 10 + '0');
+				}
+				else if (y != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+				else
+					_putchar(z + '0');
+				if (y != n)
+				{
+					_putchar(',');
+					_putchar(' ');													}
+																			}
+					_putchar('\n');
+					x++;
+				}
 }
+
