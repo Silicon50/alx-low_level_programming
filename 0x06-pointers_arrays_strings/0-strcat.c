ee75805a00cd8ae171c94a1c60c3c1abc32785ab
Silicon50 <ndukwechukwuaja@gmail.com>
2022-09-22 01:04:37 +0100

    Write a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,40 @@
 #include "main.h"
-
 /**
-*_strcat - concatenates two strings
-*@dest: A pointer to a character that will be changed
-*@src: A pointer to a character that will also be changed
-
- *Return: dest
+ * *_strncat - concatenate two strings
+ * @dest: char
+ * @src: char
+ * @n: int
+ *
+ * Description -
+ * Return: char
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i, j, k;
+	int i;
+	int j;
 
-	i = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+
+	for (j = 0; src[j] != '\0'; j++)
+
+	if (n > j)
 	while (dest[i] != '\0')
 	{
+		for (k = i; src[k - 1] != '\0'; k++)
+			dest[k] = src[k - 1];
+		dest[k] = '\0';
 		i++;
 	}
-
-	j = 0;
-	while (src[j] != '\0')
+	else
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
-		j++;
-		i++;
+		for (k = i; k < n + i; k++)
+			dest[k] = src[k - 1];
+		dest[i + j] = src[j];
 	}
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 
 	return (dest);
 }
