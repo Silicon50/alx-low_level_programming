acd76d4a6f69b87b6e997da4fb1cd43b85dd9af7
Silicon50 <ndukwechukwuaja@gmail.com>
2022-09-22 01:32:37 +0100

    concatenate 2 string 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,38 +1,30 @@
 #include "main.h"
+
 /**
- * *_strncat - concatenate two strings
- * @dest: char
- * @src: char
- * @n: int
- *
- * Description -
- * Return: char
+ *_strncat - concatinate 2 strings.
+ *@dest: first string.
+ *@src: second string.
+ *@n: the number of bytes to use from src.
+ *Return: string.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k;
+	int i = 0, k = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-
-	for (j = 0; src[j] != '\0'; j++)
-
-	if (n > j)
 	while (dest[i] != '\0')
+		i++;
+
+	while (src[k] != '\0' && n > k)
 	{
-		for (k = i; src[k - 1] != '\0'; k++)
-			dest[k] = src[k - 1];
-		dest[k] = '\0';
+		dest[i] = src[k];
+		k++;
 		i++;
 	}
-	else
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	if (n > 0)
 	{
-		for (k = i; k < n + i; k++)
-			dest[k] = src[k - 1];
-		dest[i + j] = src[j];
+		dest[i] = '\0';
 	}
-	dest[i + j] = '\0';
 
 	return (dest);
 }
