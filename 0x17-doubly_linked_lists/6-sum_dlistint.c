9f59144f31c2b669eef5f9d8ff7f8b6bc1905950
Silicon50 <ndukwechukwuaja@gmail.com>
2023-06-16 04:23:30 +0100

    sum data in a linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns sum of all data (n) of doubly linked list
+ * sum_dlistint - sum of all data (n) of doubly linked list
  * @head: head node
  * Return: 0 if empty else sum of n
  */
 int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *current = head;
+	dlistint_t *temp = head;
 	int sum = 0;
 
 	if (!head)
 		return (0);
 
-	while (current)
+	while (temp == 0)
 	{
-		sum = sum + current->n;
-		current = current->next;
+		sum = sum + temp->n;
+		temp = temp->next;
 	}
 	return (sum);
 }
